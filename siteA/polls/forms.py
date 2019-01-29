44c8c4f11c9364d2b4b36bbdae04bf77fca50396
LEE Joon-sang <jslee.kor@gmail.com>
2019-01-29 10:20:28 +0900

    completed in basic

--- siteA/polls/forms.py
+++ siteA/polls/forms.py
@@ -4,5 +4,4 @@ class ContactForm(forms.Form):
     subject = forms.CharField(max_length=100)
     message = forms.CharField(widget=forms.Textarea)
     sender = forms.EmailField()
-    cc_myself = forms.BooleanField(required=False)
-    
+    cc_myself = forms.BooleanField(required=False)  
--- siteA/polls/myviews.py
+++ siteA/polls/myviews.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from polls.models import Question, Choice
 from django.views import generic
+from django.template.context_processors import request
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
@@ -16,7 +17,9 @@ class ChoiceView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
     
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
   
 class ResultView(generic.DetailView):
-    moodel = Question
+    model = Question
     template_name = 'polls/results.html'
